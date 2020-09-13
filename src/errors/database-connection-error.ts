9dfa5bba04ec2553b5f24fb7a05b44813eb9f056
vaddv <vijay.vaddem@gmail.com>
2020-09-13 12:03:05 +0530

    IMplemented a defined contract within error handlers which will be used by error middleware. This way, error handler middleware can handle any number of types of errors

--- src/errors/database-connection-error.ts
+++ src/errors/database-connection-error.ts
@@ -2,10 +2,15 @@ import { ValidationError } from "express-validator";
 
 export class DatabaseConnectionError extends Error {
   reason = "Error connecting to database";
+  statusCode = 500;
 
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -9,15 +9,10 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    console.log("Handling as Request validation error");
-    const formattedErrors = err.errors.map((error) => {
-      return { message: error.msg, field: error.param };
-    });
-    return res.status(400).send({ errors: formattedErrors });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
   if (err instanceof DatabaseConnectionError) {
-    //console.log("Handling as database connection error");
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
