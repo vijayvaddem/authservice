18a625192783e3be62784dc96fc25424fcdc4467
vaddv <vijay.vaddem@gmail.com>
2020-09-18 23:55:44 +0530

    Adding a new user during sign up

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -24,10 +25,18 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user now");
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      console.log("Email in use");
+      res.send({});
+    }
+
+    console.log("Creating new user");
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(200).send(user);
   }
 );
 
